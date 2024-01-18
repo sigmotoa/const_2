446d4a21b60ace6c3bcab1766b2bfc98e1e057cc
sigmotoa <si.galvismotoa@gmail.com>
2024-01-17 19:25:51 -0500

    Enums, Status and more models

--- hello.py
+++ hello.py
@@ -1,6 +1,48 @@
-from fastapi import FastAPI, Query, Path, Body
+from fastapi import FastAPI, Query, Path, Body, status
 from typing import Optional
 from pydantic import BaseModel, Field
+from enums import HairColor, Zodiac
+
+class Person(BaseModel):
+    first_name:str=Field(
+        ...,
+        min_lenght=2,
+        max_lenght=30,
+        example="sigmotoa"
+        )
+    last_name:str=Field(
+        ...,
+        min_lenght=2,
+        max_lenght=30,
+        example="sigmotoa"
+        )
+    age:int = Field(
+        ...,
+        gt=0,
+        le=140,
+        example=54
+        )
+    hair_color:Optional[HairColor] = Field(
+        default=None,
+        example=HairColor.BLONDIE
+        )
+    zodiac:Optional[Zodiac]=Field(
+        default=Zodiac.geminis
+        )
+    married:Optional[bool]=Field(
+        default=None,
+        example=False
+        )
+
+
+class PersonOut(Person):
+    pass
+
+class PersonIn(Person):
+    password:str=Field(
+        ...,
+        min_length=5
+        )
 
 class User(BaseModel):
     user_id : int
@@ -10,6 +52,12 @@ class User(BaseModel):
     user_status : Optional [bool]
 
 app = FastAPI()
+
+
+@app.post("/person/new", response_model=PersonOut, status_code=status.HTTP_201_CREATED)
+def create_person(person:PersonIn=Body(...)):
+    return person
+
 
 @app.put("/user/show/{user_id}")
 def show_user(
@@ -45,7 +93,7 @@ def age(user:str, year:int):
     return{user + " your age is: "+(str)(2024-year)}
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 def hello():
     return {"Hello":"sigmotoa says"}
 
